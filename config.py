3601e752f0427c3f249fd06504d85497d28852bb
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-07-22 21:23:20 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 class Config:
-    API_ID=25826048
-    API_HASH="b486ee260537697fdfc56b2b61cbc049"
+    API_ID=24037348
+    API_HASH="f61ab53e51d34e45eb6e04051f010fb3"
     TOKEN="6800400717:AAGnF3cVxDlv9owwl34m8O-VE98vxkPEZx8"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "420401302").split(" "))
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "7307974073").split(" "))
     START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
     BOT_ID=6800400717
     BOT_USERNAME="DekjawXRobot"
