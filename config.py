2ee34c0e865753bbfd8dbf403df9abdbff223adf
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-07-22 20:56:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID=24509589
-    API_HASH="717cf21d94c4934bcbe1eaa1ad86ae75"
-    TOKEN="6572403691:AAFV-3PpCb7N06uqJC2mCdK4QzW6VtV5CeE"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "6664582540").split(" "))
+    API_ID=25826048
+    API_HASH="b486ee260537697fdfc56b2b61cbc049"
+    TOKEN="6800400717:AAGnF3cVxDlv9owwl34m8O-VE98vxkPEZx8"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "420401302").split(" "))
     START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
-    BOT_ID=6796545941
-    BOT_USERNAME="ZuliaiBot"
-    BOT_NAME="ZuliaiBot"
+    BOT_ID=6800400717
+    BOT_USERNAME="DekjawXRobot"
+    BOT_NAME="Dante"
